992271daa97ee84965e575ee19ff2ca8046f1d52
federico-dibitonto <f.dibitonto94@gmail.com>
2024-04-10 17:48:10 +0200

    fixed trains_db

---
+++ database/migrations/2024_04_10_151008_add_info_to_trains_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code');
+            $table->integer('number_of_carriages');
+            $table->boolean('in_time');
+            $table->boolean('deleted');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('company', 'departure_station', 'arrival_station', 'departure_time', 'arrival_time', 'train_code', 'number_of_carriages', 'in_time', 'deleted');
+        });
+    }
+};
